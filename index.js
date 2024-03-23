5deadd4f0b374d2e49e6bda51075639511a9b3c7
mkhadimhussain <163546834+mkhadimhussain@users.noreply.github.com>
2024-03-24 03:25:22 +0500

    simple calculator

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number:", type: "number", name: "firstNumber" },
+    { message: "Enter second number:", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"], //list
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") { //take the name: operator from answer
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operator.");
+}
